e0e08d1ce49a3122755be3167c041a53bb3027f8
danieljappe <danieljappe@icloud.com>
2023-11-20 11:38:44 +0100

    init commit

---
+++ src/main/java/com/example/vecchiabackend/VecchiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vecchiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VecchiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VecchiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vecchiabackend/VecchiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vecchiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VecchiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
