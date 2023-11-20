11b22c6fd65fcaf8834a8a960c0464a5131fab38
danieljappe <danieljappe@icloud.com>
2023-11-20 15:39:07 +0100

    Tilføjet packages + modeller

---
+++ src/main/java/com/example/vecchiabackend/model/Employee.java
@@ -0,0 +1,40 @@
+package com.example.vecchiabackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Employees_EmployeeID")
+    private int employeeID;
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Phone")
+    private String phone;
+
+    @Column(name = "IsAdmin")
+    private boolean isAdmin;
+
+    @Lob
+    @Column(name = "EmployeeImage", columnDefinition="BLOB")
+    private byte[] employeeImage;
+}
---
+++ src/main/java/com/example/vecchiabackend/model/MenuItem.java
@@ -0,0 +1,33 @@
+package com.example.vecchiabackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Employees")
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ItemID")
+    private int itemID;
+
+    @Column(name = "Name", length = 100)
+    private String name;
+
+    @Column(name = "Description", length = 1000)
+    private String description;
+
+    @Column(name = "Price")
+    private double price;
+
+    @Column(name = "Category", length = 50)
+    private String category;
+}
