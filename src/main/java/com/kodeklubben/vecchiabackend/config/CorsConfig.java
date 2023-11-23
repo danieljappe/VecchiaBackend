b352de5a035ec965b85228c2c6e3710e6b6ec0d0
danieljappe <danieljappe@icloud.com>
2023-11-23 13:55:07 +0100

    frontend added to allowed origin

---
+++ src/main/java/com/kodeklubben/vecchiabackend/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.kodeklubben.vecchiabackend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://vecchia-78929.web.app/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
