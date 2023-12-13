08ef5c4d9daa8f9439a2c179894de8f5f99e49be
fred077a <fred18q3@stud.kea.dk>
2023-12-13 21:41:32 +0100

    added User as dto

--- src/main/java/com/kodeklubben/vecchiabackend/config/SecurityConfig.java
+++ src/main/java/com/kodeklubben/vecchiabackend/config/SecurityConfig.java
@@ -78,10 +78,9 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("https://vecchiabackend.azurewebsites.net", "https://vecchia-78929.web.app/", "https://127.0.0.1:5500"));
-    // ... other CORS configurations
     configuration.addAllowedOrigin("https://vecchia-78929.web.app");
     configuration.addAllowedOrigin("https://127.0.0.1:5500");
+    configuration.addAllowedOrigin("https://localhost:5500");
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
 
